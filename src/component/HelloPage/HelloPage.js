251c023b8820bf63713f437d0710e07d4c4c62db
E1442020 <114053062+E1442020@users.noreply.github.com>
2023-01-10 13:40:20 +0200

    login

--- src/component/HelloPage/HelloPage.js
+++ src/component/HelloPage/HelloPage.js
@@ -6,7 +6,7 @@ export default function HelloPage() {
   return (
     <> <div className='register-login-container'>
     <div className='register-content'>
-    <h2>Success <AiOutlineCheck size={30} /> </h2>
+    <h2 className='finish'>Success <AiOutlineCheck size={30} /> </h2>
         <h2>Hello </h2>
         </div>
         </div></>
