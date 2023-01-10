94c8bcc07f269b77ae42cb9c2343e71154a86c64
E1442020 <114053062+E1442020@users.noreply.github.com>
2023-01-10 18:35:47 +0200

    login

--- src/component/Register/Register.js
+++ src/component/Register/Register.js
@@ -5,23 +5,37 @@ import "./Register.css";
 export default function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const[fullName,setFullName]=useState("")
+  const [fullName, setFullName] = useState("");
+  const [phone, setPhone] = useState("");
   const linkRef = useRef("");
-  const[confirmField,setConfirmField] = useState(true)
+  const [confirmField, setConfirmField] = useState(false);
 
   const handleClickRegisterForm = (e) => {
     e.preventDefault();
-    if (email.length > 0 && password.length > 0) {
-        setConfirmField(true)
+    if (
+      email.length <= 0 ||
+      password.length <= 0 ||
+      fullName.length <= 0 ||
+      phone.length <= 0
+    ) {
+      setConfirmField(true);
+    } else {
+      setConfirmField(false);
+    }
+
+    if (
+      email.length > 0 &&
+      password.length > 0 &&
+      fullName.length > 0 &&
+      phone.length > 0
+    ) {
       localStorage.setItem("emailValue", email);
       localStorage.setItem("passwordValue", password);
       localStorage.setItem("fullNameValue", fullName);
       linkRef.current.click();
-     
-    }else{
-        setConfirmField(false);
     }
   };
+
   return (
     <>
       <div className="register-login-container">
@@ -29,10 +43,17 @@ export default function Register() {
           <h2>Register</h2>
           <form className="register-form" onSubmit={handleClickRegisterForm}>
             <label>Name</label>
-            <input type="text" placeholder="Enter Full Name" 
-            value={fullName} 
-            onChange={(e) => setFullName(e.target.value)}/>
-            <span className="confirmField">{!confirmField&&'this field can’t be empty'}</span>
+            <input
+              type="text"
+              placeholder="Enter Full Name"
+              value={fullName}
+              onChange={(e) => setFullName(e.target.value)}
+            />
+            <span className="confirmField">
+              {confirmField && fullName.length <= 0
+                ? "this field can’t be empty"
+                : ""}
+            </span>
             <label>Email</label>
             <input
               type="email"
@@ -40,7 +61,11 @@ export default function Register() {
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             />
-            <span className="confirmField">{!confirmField&&'this field can’t be empty'}</span>
+            <span className="confirmField">
+              {confirmField && email.length <= 0
+                ? "this field can’t be empty"
+                : ""}
+            </span>
 
             <label>Password</label>
             <input
@@ -49,15 +74,33 @@ export default function Register() {
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
-            <span className="confirmField">{!confirmField&&'this field can’t be empty'}</span>
+            <span className="confirmField">
+              {confirmField && password.length <= 0
+                ? "this field can’t be empty"
+                : ""}
+            </span>
 
             <label>Phone no</label>
-            <input type="number" placeholder="Enter Contact no" />
-            <span className="confirmField">{!confirmField&&'this field can’t be empty'}</span>
+            <input
+              type="number"
+              placeholder="Enter Contact no"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+            <span className="confirmField">
+              {confirmField && phone.length <= 0
+                ? "this field can’t be empty"
+                : ""}
+            </span>
 
-            <button type="submit"><Link to= "/"></Link>Register</button>
+            <button type="submit">
+              <Link to="/"></Link>Register
+            </button>
             <p>
-              Already register <Link to="/" ref={linkRef}>login in?</Link>
+              Already register{" "}
+              <Link to="/" ref={linkRef}>
+                login in?
+              </Link>
             </p>
           </form>
         </div>
